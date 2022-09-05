63ef56f1dc898a13d99782a8120a32e8a5b4e798
SANDRO SIMON <sandrosimonore@gmail.com>
2022-09-05 01:21:41 -0500

    more changes in slider (this is complicated...)

--- src/components/CategoriesSlider.js
+++ src/components/CategoriesSlider.js
@@ -1,32 +1,16 @@
 import './CategoriesSlider.scss';
 import Category from './Category';
 import useGetCategories from '../hooks/useGetCategories';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function CategoriesSlider(){
     const categories = useGetCategories();
-    // const [offsets, setOffsets] = useState([]);
-    // const [totalWidth, setTotalWidth] = useState(0);
-    const [currentPoint, setCurrentPoint] = useState(0);
+    const firstVisible = useRef(0);
+    const [elements, setElements] = useState([]);
     
     useEffect(()=> {
         
-        let sumWidth = 0;
-        const tempArray = [...document.getElementById('categories').getElementsByClassName('category')].map((element, index) => {
-            
-            const currentWidth = Number(window.getComputedStyle(element).getPropertyValue('width').replace('px',''))
-            sumWidth +=  currentWidth; 
-            
-            return {
-                id: element.id,
-                offset: sumWidth
-            }
-            
-        });
-
-        // setOffsets(tempArray);
-        // setTotalWidth(sumWidth);
-
+        setElements(document.getElementById('categories').getElementsByClassName('category'))    
         
         // INTERSECTION OBSERVER
         let options = {
@@ -36,84 +20,80 @@ function CategoriesSlider(){
 
         const callback = (entries) => {
             entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    //console.log(entry.target.id)
-                    //entry.target.
-                    document.getElementById('left-arrow').classList.add('hidden')
-
-                } else {
-                    document.getElementById('left-arrow').classList.remove('hidden') 
+                
+                if (entry.target.classList.contains('first')) {
+                    
+                    if (entry.isIntersecting ) {
+                        document.getElementById('left-arrow').classList.add('hidden')
+                        console.log('...')
+                    } else {
+                        document.getElementById('left-arrow').classList.remove('hidden')
+                    }
+                } else if (entry.target.classList.contains('last')) {
+                    if (entry.isIntersecting ) {
+                        document.getElementById('right-arrow').classList.add('hidden')
+                    } else {
+                        document.getElementById('right-arrow').classList.remove('hidden')
+                    }
                 }
+
             }); 
         }
 
         let observer = new IntersectionObserver(callback, options);
 
-        observer.observe(document.getElementById('37'));
-        observer.observe(document.getElementById('28'));
-
+        observer.observe(document.getElementsByClassName('first')[0]);
+        observer.observe(document.getElementsByClassName('last')[0]);
         // FIN INTERSECTION OBSERVER
             
     }, [])
     
-    
-
     const clickArrowsHandler = (e) => {
 
-        let box = document.getElementById('categories');
-        const categoriesWidth = window.getComputedStyle(box).getPropertyValue('width').replace('px','');
-        box = document.getElementById('categories-container');
-        const categoriesContainerWidth = window.getComputedStyle(box).getPropertyValue('width').replace('px','');
-        
-        
-        //console.log(categoriesWidth)
-        //console.log(categoriesContainerWidth)
-        const percentage = Number((categoriesContainerWidth / categoriesWidth * 100).toFixed(2));
-        //console.log(percentage)
-
-        let points = [0];
-
-        while (points.at(-1) < 100) {
-            //console.log(points.at(-1))
-            points.push(points.at(-1) + percentage > 100 ? 100 :  points.at(-1) + percentage)
-        }
-
-        console.log(points)
-        
         if (e.target.id === 'left-arrow') {
-            
-        } else if (e.target.id === 'right-arrow') {
-            setCurrentPoint(previous => previous + 1)
-            document.getElementById('categories').style.transform = `translate(-${points[currentPoint]}%)`;
-        }
 
-        //const percentage = offsets[currentOffset].offset / totalWidth * 100
-        //console.log(percentage)
+            firstVisible.current--;
+            elements[firstVisible.current].classList.remove('hidden');
         
+        } else if (e.target.id === 'right-arrow') {
         
+            elements[firstVisible.current].classList.add('hidden');
+            firstVisible.current++;
+        }
+                
     }
     
     return (
 
         <div className='categories-slider'>
-            <div className='arrow left-arrow' id='left-arrow' onClick={clickArrowsHandler}>
-                &#8249;
+            <div className='arrow-container'>
+                <div className='arrow left-arrow' id='left-arrow' onClick={clickArrowsHandler}>
+                    &#8249;
+                </div>
             </div>
+            
             <div className='categories-container' id='categories-container'>
                 <div className='categories' id='categories'>
 
                     {   
                         categories && (   
                             
-                            categories.map(category => {
-                                return <Category key={category.id} category={category} />
+                            categories.map((category, index, array) => {
+                                return <Category 
+                                    key={category.id} 
+                                    category={category}
+                                    isFirst={(index === 0) && true}
+                                    isLast={(index === array.length - 1) && true}    
+                                />
                             })
                         )
                     }
                 </div>
             </div>
-            <div className='arrow right-arrow' id='right-arrow' onClick={clickArrowsHandler}>
-                &#8250;
+            <div className='arrow-container'>
+                <div className='arrow right-arrow' id='right-arrow' onClick={clickArrowsHandler}>
+                    &#8250;
+                </div>
             </div>
         </div>
     )
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,11 @@
 import './Category.scss'
 function Category(props){
     const {name, id} = props.category;
+    let position = '';
+    if (props.isFirst) position = 'first';
+    if (props.isLast) position = 'last';
     return(
-        <div className='category' id={id}>
+        <div className={`category ${position}`} id={id}>
             <p>{name}</p> 
         </div>
     )
