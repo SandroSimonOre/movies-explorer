944d61531a8500a88f9c8a17d347b99f0450bae1
SANDRO SIMON <sandrosimonore@gmail.com>
2022-09-08 11:03:23 -0500

    Slider categories removed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,9 @@
 
-import Categories from './CategoriesSlider.js';
 import CardsContainer from './CardsContainer.js';
 import './Main.scss';
 const Main = () => {
     return (
         <div className='main'>
-            <Categories />
             <CardsContainer />
         </div>
     )
