5e8e03f48c896c9676ace9a76d16ef4777a60c5e
SANDRO SIMON <sandrosimonore@gmail.com>
2022-09-09 11:12:57 -0500

    Slider fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
-
+import CategoriesSlider from './CategoriesSlider.js';
 import CardsContainer from './CardsContainer.js';
 import './Main.scss';
 const Main = () => {
     return (
         <div className='main'>
+            <CategoriesSlider />
             <CardsContainer />
         </div>
     )
