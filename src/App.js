5d67ad53e35fbd8984e3376e692a36c62216f639
SANDRO SIMON <sandrosimonore@gmail.com>
2022-09-22 11:56:23 -0500

    Problems with Sidebar fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import {Header} from './components/Header';
-import {Sidebar} from './containers/Sidebar';
+import {Sidebar} from './components/Sidebar';
 import {Main} from './containers/Main';
-
 import {Discovering} from './containers/Discovering';
 import {Favorites} from './containers/Favorites';
 import {Trending} from './containers/Trending';
@@ -11,75 +10,60 @@ import {Searching} from "./containers/Searching";
 import './App.scss'
 
 export const App = () => {
+    
     return (
         <div id='app' className='app'>  
 
             <Routes>
 
-                <Route exact path='/' element = {<Navigate to = '/favorites' />} />
-                <Route exact path='/favorites' element = { <FavoriteComponent /> } />
-                <Route exact path='/trending' element = { <TrendingComponent /> } />
-                <Route exact path='/discovering' element = { <DiscoveringComponent /> } />
-                <Route exact path='/search' element = { <SearchingComponent /> } />
+                <Route path='/' element = {<Navigate to = '/favorites' />} />
+                
+                <Route path='/favorites' element = { 
+                        <> 
+                            <Header />
+                            <Sidebar />
+                            <Main>
+                                <Favorites />
+                            </Main>
+                        </>
+                    }
+                 />
+                
+                <Route path='/trending' element = { 
+                        <> 
+                            <Header />
+                            <Sidebar />
+                            <Main>
+                                <Trending />
+                            </Main>
+                        </>
+                    } 
+                />
+                
+                <Route path='/discovering' element = {
+                        <> 
+                            <Header />
+                            <Sidebar />
+                            <Main>
+                                <Discovering />
+                            </Main>
+                        </>
+                    } 
+                />
+                
+                <Route path='/search' element = { 
+                        <> 
+                            <Header />
+                            <Sidebar />
+                            <Main>
+                                <Searching />
+                            </Main>
+                        </>
+                    } 
+                />
                 
             </Routes>
 
         </div> 
-    )
-}
-
-
-const DiscoveringComponent = () => {
-    
-    return (
-        <>
-            <Header />
-            <Sidebar />
-            <Main>
-                {/* <CategoriesSlider /> */}
-                <Discovering />
-            </Main>
-        </>
-    )
-}
-
-const TrendingComponent = () => {
-    return (
-        <>
-            <Header />
-            <Sidebar />
-            <Main>
-                {/* <CategoriesSlider /> */}
-                <Trending />
-            </Main>
-        </>
-    )
-}
-
-const FavoriteComponent = () => {
-    return (
-        <>
-            <Header />
-            <Sidebar />
-            <Main>
-                {/* <CategoriesSlider /> */}
-                <Favorites />
-            </Main>
-            
-        </>
-    )
-}
-
-const SearchingComponent = () => {
-    
-    return (
-        <>
-            <Header />
-            <Sidebar />
-            <Main>
-                <Searching key='ola' />
-            </Main>
-            
-        </>
     )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,28 +1,29 @@
-import { Link } from "react-router-dom";
-import {MenuItem} from '../components/MenuItem'
+import { NavLink } from "react-router-dom";
+import {MenuItem} from './MenuItem'
 import './Sidebar.scss'
 export const Sidebar = () => {
 
     return (
         
         <div id='sidebar' className='sidebar'>
+            
             <div className='menu-container'>
                 
-                <Link className='link' to = '/favorites'>
+                <NavLink className='link' to = '/favorites'>
                     <MenuItem title = 'Favorites' url = 'https://cdn-icons-png.flaticon.com/512/3507/3507694.png' />
-                </Link>
+                </NavLink>
 
-                <Link className='link' to = '/trending'>
+                <NavLink className='link' to = '/trending'>
                     <MenuItem title = 'Trending' url = 'https://cdn-icons-png.flaticon.com/512/3121/3121768.png' />
-                </Link>
+                </NavLink>
                 
-                <Link className='link' to = '/discovering'>
+                <NavLink className='link' to = '/discovering'>
                     <MenuItem title = 'Discovering' url = 'https://cdn-icons-png.flaticon.com/512/3649/3649527.png' />
-                </Link>
+                </NavLink>
             
-                <Link className='link' to = '/search'>
+                <NavLink className='link' to = '/search'>
                     <MenuItem title = 'Search' url = 'https://cdn-icons-png.flaticon.com/512/1055/1055645.png' />
-                </Link>
+                </NavLink>
 
             </div>
 
